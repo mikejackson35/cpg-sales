6b80402e595ab7465fcae148bc8129016449ea37
Mike_Jackson <mikejackson35@gmail.com>
2024-01-19 11:32:47 -0800

    commit

--- pages/viz_analysis.py
+++ pages/viz_analysis.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import pygwalker as pyg
+import streamlit.components.v1 as components
 
 st.set_page_config(page_title='Main Page',
                    page_icon=":bar_chart:",
@@ -30,17 +31,17 @@ df = get_data_from_csv()
 ### MASTER DATA ###
 all_sales = df.copy()
 
-# invoice date cleanup
-all_sales['Invoice Date'] = pd.to_datetime(all_sales['Invoice Date'])
-all_sales['Invoice Date'] = all_sales['Invoice Date'].dt.normalize()
-all_sales['Invoice Date'] = all_sales['Invoice Date'].dt.floor('D')
+# date cleanup
+all_sales['date'] = pd.to_datetime(all_sales['date'])
+all_sales['date'] = all_sales['date'].dt.normalize()
+all_sales['date'] = all_sales['date'].dt.floor('D')
 
 # st.markdown("<h1 style='text-align: center; color: orange;'>Visualize Data Here</h1>", unsafe_allow_html=True)
 pyg_html = pyg.to_html(all_sales)
 
 # pyg_html = pyg.walk(all_sales, dark='light', return_html=True)
 
-components.html(pyg_html, height=1000, width=1500,scrolling=True)
+components.html(pyg_html, height=1000, width=1700,scrolling=True)
 
 # ---- REMOVE UNWANTED STREAMLIT STYLING ----
 hide_st_style = """
