44b13db0550238a9d9730c86d72113216176cfa6
Mike_Jackson <mikejackson35@gmail.com>
2024-01-26 08:04:46 -0800

    update

--- pages/main_2023.py
+++ pages/main_2023.py
@@ -184,50 +184,38 @@ other_22 = all_sales[(all_sales['date'].dt.year == 2022) & (all_sales['market_se
 yoy_other = int(other_23-other_22)
 yoy_other_perc = round(int(other_23-other_22) / other_22,2)
 
-samples_23 = int(all_sales[(all_sales.date.dt.year==2023) & (all_sales.market_segment=='Samples')]['qty'].sum())
-samples_22 = int(all_sales[(all_sales.date.dt.year==2022) & (all_sales.market_segment=='Samples')]['qty'].sum())
-yoy_samples = int(samples_23-samples_22)
-yoy_samples_perc = round(int(samples_23-samples_22)/(1 + samples_22),2)
-
-outlet_23 = all_sales[(all_sales['date'].dt.year == 2023) & (all_sales['market_segment'] == 'Outlet')].usd.sum()
-outlet_22 = all_sales[(all_sales['date'].dt.year == 2022) & (all_sales['market_segment'] == 'Outlet')].usd.sum()
-yoy_outlet = int(outlet_23-outlet_22)
-yoy_outlet_perc = round(int(outlet_23-outlet_22) / outlet_22,2)
-
 # BEGIN ROWS AND COLUMNS METRICS
-blank,col1, col2, col3, col4, col5,blank = st.columns((.5,2,2,2,2,2,.5))
+blank,col1, col2, col3, col4, blank = st.columns((1,2,2,2,1))
 
 blank.markdown("")
 col1.metric(label='Vending', value=f"${int(vending_23):,}", delta = f"{yoy_vend_perc:.0%}")
 col2.metric(label='Online', value=f"${int(online_23):,}", delta = f"{yoy_online_perc:.0%}")
 col3.metric(label='Alternate Retail', value=f"${int(alt_23):,}", delta = f"{yoy_alt_perc:.0%}")
 col4.metric(label='Canada', value=f"${int(canada_23):,}", delta = f"{yoy_canada_perc:.0%}")
-col5.metric(label='Convenience', value=f"${int(conv_23):,}", delta = f"{yoy_conv_perc:.0%}")
 blank.markdown("")
 
 st.markdown("##")
 st.markdown("##")
 
-blank,col1, col2, col3, col5,blank = st.columns((1,2,2,2,2,1))
+blank,col1, col2, col3, col4, blank = st.columns((1,2,2,2,1))
 
 blank.markdown("")
-col1.metric(label='Grocery', value=f"${int(grocery_23):,}", delta = f"{yoy_grocery_perc:.0%}")
-col2.metric(label='Broadline', value=f"${int(broadline_23):,}", delta = f"{yoy_broadline_perc:.0%}")
-col3.metric(label='Other', value=f"${int(other_23):,}", delta = f"{yoy_other_perc:.0%}")
-# col4.markdown("")#metric(label='Outlet', value = f"${int(outlet_23):,}", delta = f"{yoy_outlet_perc:.0%}")
-col5.metric(label='Sample Cases', value=f"{int(samples_23):,}", delta = f"{yoy_samples_perc:.0%}")
+col1.metric(label='Convenience', value=f"${int(conv_23):,}", delta = f"{yoy_conv_perc:.0%}")
+col2.metric(label='Grocery', value=f"${int(grocery_23):,}", delta = f"{yoy_grocery_perc:.0%}")
+col3.metric(label='Broadline', value=f"${int(broadline_23):,}", delta = f"{yoy_broadline_perc:.0%}")
+col4.metric(label='Other', value=f"${int(other_23):,}", delta = f"{yoy_other_perc:.0%}")
 blank.markdown("")
 
 
 
 
 # ---- REMOVE UNWANTED STREAMLIT STYLING ----
-# hide_st_style = """
-#             <style>
-#             Main Menu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#             </style>
-#             """
+hide_st_style = """
+            <style>
+            Main Menu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
             
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
