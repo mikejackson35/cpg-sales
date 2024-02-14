d18a759d65a453894e1db05f044474bf0b26d757
Mike_Jackson <mikejackson35@gmail.com>
2024-02-14 00:47:25 -0800

    update

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ st.markdown("""
 }
 
 [data-testid="stVerticalBlock"] {
-    padding-left: 25rem;
-    padding-right: 25rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
 }
 
 [data-testid="stMetric"] {
