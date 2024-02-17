4fa69ef80d520459ce59e186cfe0a348246783d9
Mike_Jackson <mikejackson35@gmail.com>
2024-02-17 13:31:06 -0800

    update

--- main.py
+++ main.py
@@ -17,14 +17,10 @@ st.set_page_config(page_title='Awake YTD',
 
 alt.themes.enable("dark")
 st.sidebar.title("AWAKE")
-#######################
-# CSS styling for metrics
+
 st.markdown("""
 <style>
-
 [data-testid="block-container"] {
-    # padding-left: 5rem;
-    # padding-right: 20rem;
     padding-top: 1rem;
     padding-bottom: 0rem;
     margin-bottom: -7rem;
@@ -44,14 +40,14 @@ st.markdown("""
 }
 
 [data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: 900;
+display: flex;
+justify-content: center;
+align-items: center;
+font-weight: 900;
 }
             
 [data-testid="stMetricValue"] {
-  font-size: 20px;
+font-size: 20px;
 }
 
 [data-testid="stMetricDeltaIcon-Up"] {
@@ -89,11 +85,11 @@ st.markdown("""
 [aria-selected="true"] {
     background-color: #A29F99;
 }
+        </style>
+        """, unsafe_allow_html=True)
 
-</style>
-""", unsafe_allow_html=True)
-
-# ---- PULL IN DATA FROM POSTGRES DB ----
+###############
+# L2 CONNECTION
 @st.cache_data
 def get_connection():
     conn = st.connection('dot', type ="sql")
@@ -101,12 +97,30 @@ def get_connection():
     return all_sales
 
 all_sales = get_connection()
-
-# date cleanup
 all_sales['date'] = pd.to_datetime(all_sales['date'])
 all_sales['date'] = all_sales['date'].dt.normalize()
 all_sales['date'] = all_sales['date'].dt.floor('D')
 
+origin_dict = {'unl':'Unleashed',
+               'dot':'Dot'}
+
+all_sales['sale_origin'] = all_sales['sale_origin'].map(origin_dict)
+
+###############
+# L1 CONNECTION
+@st.cache_data
+def get_connection2():
+    conn2 = st.connection('dot', type ="sql")
+    l1 = conn2.query("SELECT * FROM unleashed_raw WHERE completed_date > '2022-12-31';")
+    return l1
+
+l1 = get_connection2()
+l1.completed_date = pd.to_datetime(l1.completed_date)
+l1['usd'] = l1['sub_total']*.75
+
+
+###############
+# COLOR DICTIONARIES
 market_segment_dict = {
     'Vending': 'rgb(56,149,73)',
     'Grocery': 'rgb(248,184,230)',
@@ -119,38 +133,48 @@ market_segment_dict = {
     'Samples': 'rgb(141,62,92)'}
 
 sale_origin_dict = {
-    'dot': 'rgb(81, 121, 198)',
-    'unl': 'rgb(239, 83, 80)'
+    'Dot': 'rgb(81, 121, 198)',
+    'Unleashed': 'rgb(239, 83, 80)'
 }
 
-# LOGO AND TITLE
-# st.sidebar.title("AWAKE")
-
+###############
+# L1/L2 KPI'S
 week_ago = datetime.today().date() - pd.offsets.Day(10)
 recent_sales = all_sales[(all_sales.date>week_ago) & (all_sales.market_segment!='Samples')]
 current_date = datetime.today().strftime('%Y-%m-%d')
 import datetime
 year_ago_today = datetime.datetime.today() - datetime.timedelta(days=365)
 
+l1_sales_24 = int(l1[(l1['completed_date'] > '2023-12-31') & (l1['completed_date'] < current_date)].usd.sum())
+l1_sales_23 = int(l1[(l1['completed_date'] > '2022-12-31') & (l1['completed_date'].dt.date < year_ago_today.date())].usd.sum())
+l1_yoy_chg_perc = f"{(l1_sales_24/l1_sales_23-1)*100:.0f}%"
+
 sales_24 = int(all_sales[(all_sales['date'] > '2023-12-31') & (all_sales['date'] < current_date)].usd.sum())
 sales_23 = int(all_sales[(all_sales['date'] > '2022-12-31') & (all_sales['date'].dt.date < year_ago_today.date())].usd.sum())
-
 yoy_chg_perc = f"{(sales_24/sales_23-1)*100:.0f}%"
-# TOP KPI'S
-col1, col2, col3, col4 = st.columns([.6,1,1,1])
+
+col1, col2, col3, col4, col5= st.columns(5)
 with col1:
-    st.markdown("")
+    # st.markdown("")
+    st.subheader(f"${l1_sales_24/1000000:.2f}M")
+    st.write("Level 1")
 with col2:
-    st.subheader(f"${sales_24/1000000:.2f}M")
-    st.write("Year-to-Date")
+    st.subheader(f"+{l1_yoy_chg_perc}")
+    st.write("YoY")
 with col3:
     st.markdown("")
-    st.image(r"assets/Nevil.png",width=75)
+    st.image(r"assets/Nevil.png",width=65)
 with col4:
+    st.subheader(f"${sales_24/1000000:.2f}M")
+    st.write("Level 2")
+with col5:
     st.subheader(f"+{yoy_chg_perc}")
-    st.write("YoY Change")
+    st.write("YoY")
+
+st.markdown("#")
 
 ###################
+# METRICS BOXES
 
 all_sales['date'] = pd.to_datetime(all_sales['date'])
 
@@ -321,18 +345,40 @@ bar_all.update_xaxes(showgrid=False,gridcolor='gray',tickfont=dict(color='#5A585
 bar_all.update_xaxes(tickmode='array',tickvals = df.index, ticktext=df.index.strftime('<b>%a<br>%d</b>'))
 bar_all.update_layout(hoverlabel=dict(font_size=18,font_family="Rockwell"),)
 
+# LEVEL 1 DAILY BAR
+l1['usd'] = l1['sub_total']*.75
+
+l1_bar_df = l1.groupby('completed_date')['usd'].sum().reset_index().set_index('completed_date')
+l1_bar_df =round(l1_bar_df[l1_bar_df.index>'2024-01-31']).sort_index()
+
+level_1_bar = px.bar(l1_bar_df,
+                     y='usd',
+                     template='plotly_white',
+                     labels={'usd':'',
+                             'completed_date':''},
+                     height=325,
+                     text_auto=",.2s")
+
+level_1_bar.update_traces(hovertemplate = '$%{y:.2s}'+'<br>%{x:%Y-%m-%d}<br>')
+level_1_bar.update_traces(marker_color='rgb(239, 83, 80)')
+level_1_bar.update_coloraxes(showscale=False)
+level_1_bar.update_yaxes(showticklabels=False,showgrid=True,tickprefix='$',gridcolor="#B1A999",tickvals=[0,25000,50000,75000,100000],tickfont=dict(color='#5A5856', size=14))
+level_1_bar.update_xaxes(showgrid=False,gridcolor='gray',tickfont=dict(color='#5A5856', size=13),title_font=dict(color='#5A5856',size=15))
+level_1_bar.update_xaxes(tickmode='array',tickvals = l1_bar_df.index, ticktext=l1_bar_df.index.strftime('<b>%a<br>%d</b>'))
+level_1_bar.update_layout(hoverlabel=dict(font_size=18,font_family="Rockwell"))
+
 # st.markdown("")
 st.markdown("<br><br><b>FEBRUARY</b> <small>Daily Sales</small>",unsafe_allow_html=True)
 
-tab1, tab2, tab3 = st.tabs(["All", "Direct/Dot", "Market"])
+tab0, tab1, tab2, tab3 = st.tabs(["L1 - All","L2 - All", "L2 - Source", "L2 - Market"])
+with tab0:
+    st.plotly_chart(level_1_bar)
 with tab1:
     st.plotly_chart(bar_all,config=config, use_container_width=True)
 with tab2:
     st.plotly_chart(scatter_origin,config=config, use_container_width=True)
 with tab3:
     st.plotly_chart(scatter_market,config=config, use_container_width=True)
-# with tab4:
-#     st.plotly_chart(scatter_customer,config=config, use_container_width=True)
 
 
 
