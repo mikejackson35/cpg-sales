0e816eff57c0b2f82f4a21f7cb448b89d7d2d263
Mike_Jackson <mikejackson35@gmail.com>
2024-03-06 16:24:53 -0800

    update

--- pages/1 - Direct v TRUE Explained.py
+++ pages/1 - Direct v TRUE Explained.py
@@ -4,17 +4,6 @@ st.set_page_config(page_title='Direct vs TRUE',
                    page_icon='assets/Nevil.png',
                    layout='wide')
 
-st.sidebar.title('Direct Sales')
-st.sidebar.markdown('Dot Sales Realized upon Purchase From AWAKE')
-st.sidebar.title('TRUE Sales')
-st.sidebar.markdown('Dot Sales Realized upon Purchase From Dot')
-
-tab1, tab2 = st.tabs(['Direct', ':orange[TRUE]'])
-with tab1:
-    st.image(r"./assets/direct_sales.png", use_column_width='auto')
-with tab2:
-    st.image(r"./assets/TRUE_sales.png", use_column_width='auto')
-
 st.markdown("""
 <style>
 
@@ -62,3 +51,14 @@ st.markdown("""
             
 </style>
         """, unsafe_allow_html=True)
+
+st.sidebar.title('Direct Sales')
+st.sidebar.markdown('Dot Sales Realized upon Purchase From AWAKE')
+st.sidebar.title('TRUE Sales')
+st.sidebar.markdown('Dot Sales Realized upon Purchase From Dot')
+
+tab1, tab2 = st.tabs(['Direct', ':orange[TRUE]'])
+with tab1:
+    st.image(r"./assets/direct_sales.png", use_column_width='auto')
+with tab2:
+    st.image(r"./assets/TRUE_sales.png", use_column_width='auto')
