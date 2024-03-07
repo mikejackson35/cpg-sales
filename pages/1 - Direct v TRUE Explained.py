1f179d46e804f7913ced43da50499c45ba3229af
Mike_Jackson <mikejackson35@gmail.com>
2024-03-06 16:36:38 -0800

    update

--- pages/1 - Direct v TRUE Explained.py
+++ pages/1 - Direct v TRUE Explained.py
@@ -7,10 +7,6 @@ st.set_page_config(page_title='Direct vs TRUE',
 st.markdown("""
 <style>
 
-[data-testid="stAppViewContainer"] {
-    padding: 0px;
-}
-
 [data-baseweb="tab-list"] {
     gap: 4px;
 }
@@ -27,27 +23,9 @@ st.markdown("""
     font-weight:  1000;
 }
 
-[data-testid="stMarkdownContainer"] p {font-size:1.5rem;
-    }
-
-[data-baseweb="tab-list"] {
-    gap: 4px;
+.stTabs [data-baseweb="tab-list"] button [data-testid="stMarkdownContainer"] p {
+font-size:2rem;
 }
-
-[data-baseweb="tab"] {
-    height: 50px;
-    width: 625px;
-    white-space: pre-wrap;
-    background-color: #A29F99;
-    border-radius: 4px 4px 0px 0px;
-    gap: 3px;
-    padding-top: 8px;
-    padding-bottom: 8px;
-    font-weight:  1000;
-}
-            
-[data-testid="stMarkdownContainer"] p {font-size:1.5rem;
-    }
             
 </style>
         """, unsafe_allow_html=True)
